9decf591281f30b8d416178e51fa87d2d359d54e
kilendo dev <kilendo mail>
2025-12-10 05:48:52 +0100

    bon 8

--- src/VirtualPhone/VirtualPhone.js
+++ src/VirtualPhone/VirtualPhone.js
@@ -1127,17 +1127,17 @@ function ChildApi66profilFA({ api }) {
       <div className="type3">
         <div className="a"> <img onClick={GotoVideo} src={api.urlPhoto} alt=""/> </div>
         <div className="b"> <button onClick={GotoVideo}><SvgPlay2/></button> </div>
-		<div className="p-15px"> <p>{api.clic}</p> </div>
       </div> </>)}
       {/* type=3 , video  */}
 
       {api.type === "2" && id && (<> {/* type=2 , photo  */}
       <div className="type2"> 
 		  <img onClick={GotoPhoto} src={api.urlPhoto} alt=""/> 
-		  <div className="p-15px"> <p>{api.clic}</p> </div>
 	  </div> 
       </>)}
       {/* type=2 , photo  */}
+	  
+	  <div className="p-15px"> <p>{api.clic}</p> </div>
     </div>
     </>
   );
@@ -1163,8 +1163,6 @@ function ChildApi266profilFA({ api2 }) {
     });
     return newDate;
   };
-
-
 
 
   const idPersonConnectedFA = localStorage.getItem("idPersonConnectedFA");
