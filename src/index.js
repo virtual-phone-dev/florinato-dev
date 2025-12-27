b770ec15c5dc10ba6d7b4eae555fee410db4b9be
kilendo dev <kilendo mail>
2025-12-27 16:57:16 +0100

    bon 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./app/store";
+// import { Provider } from "react-redux";
+// import store from "./app/store";
 
+/*
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -11,4 +12,11 @@ root.render(
       <App />
     </Provider>
   </React.StrictMode>
+); */
+
+
+// enlève StrictMode pour tester
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
 );
+
