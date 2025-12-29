5a5cac9dd36c6efa8bc13eaaf52e04d5aba5efb0
kilendo dev <kilendo mail>
2025-12-29 09:48:20 +0100

    bon 4

--- src/utils.js
+++ src/utils.js
@@ -847,13 +847,13 @@ export function ChildApi66LesVideos({ api, photo, video }) {
 		{/* {afficherVideo && (!verifierId || id) && (<> */}
 		{afficherVideo && (<> 
 		<img className="video-thumb" src={api.urlPhoto} />
-		<pre className="pre-17px">{api.message}</pre>
+		<pre className="video-thumb">{api.message}</pre>
 		<p className="p-15px">{api.clic} clic</p> </>)}
 		
 		{/* {afficherPhoto && (!verifierId || id) && (<> */}
 		{afficherPhoto && (<> 
 		<img className="video-thumb" src={api.urlPhoto} />
-		<pre className="pre-17px">{api.message}</pre>
+		<pre className="video-thumb">{api.message}</pre>
 		<p className="p-15px">{api.clic} clic</p> </>)}
     </>
   );
