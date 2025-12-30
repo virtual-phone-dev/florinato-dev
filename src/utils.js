a061793c2d008024468c44f0c7ed16390027d230
kilendo dev <kilendo mail>
2025-12-30 06:26:19 +0100

    bon 4

--- src/utils.js
+++ src/utils.js
@@ -846,9 +846,10 @@ export function ChildApi66LesVideos({ api, photo, video }) {
     <>
 		{/* {afficherVideo && (!verifierId || id) && (<> */}
 		{afficherVideo && (<> 
+		<div className="video-card">
 		<img className="video-thumb" src={api.urlPhoto} /> 
 		<pre className="pre-17px">{api.message}</pre>
-		<p className="p-15px">{api.clic} clic</p> </>)}
+		<p className="p-15px">{api.clic} clic</p> </div></>)}
 		
 		{/* {afficherPhoto && (!verifierId || id) && (<> */}
 		{afficherPhoto && (<> 
@@ -863,13 +864,12 @@ export function ChildApi66LesVideos({ api, photo, video }) {
 export function LesVideos({ data = [], setId, clicVideo, voirVideo, video }) {
   return (
 <div className="video-grille">
-<div className="video-card"> 
+ 
 	{data.map((api) => (
 	  <div onClick={() => { localStorage.setItem("urlVideo", api.urlVideo); setId(api._id); voirVideo(api); clicVideo({ id:api._id, idOther:api.idAccountChef }) }}>
 		<ChildApi66LesVideos api={api} video />
 	  </div>
 	  ))}
-</div>
 </div>
 )}
 
