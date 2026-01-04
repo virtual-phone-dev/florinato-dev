c51d876884bfaa01aec8d247793b1d83d81b5b2b
kilendo dev <kilendo mail>
2026-01-04 08:44:23 +0100

    bon 4

--- src/VirtualPhone/VirtualPhone.js
+++ src/VirtualPhone/VirtualPhone.js
@@ -52844,12 +52844,14 @@ g
 				<video ref={videoRef} autoPlay muted loop controls> <source src={urlVideo} type="video/mp4"/> </video>
 			</div>
 			
-			<p className="p-14px-ccc">Commenter</p>
-			<p className="p-14px-ccc">Modifier le post</p>
-			<p className="p-14px-ccc">Supprimer le post</p>
-			<p onClick={ModifierTitrePageFA} className="p-14px-ccc">Modifier le Titre</p>
-			<p onClick={ChangerMiniaturePage} className="p-14px-ccc">Changer la Miniature</p>
-			<p onClick={ModifierUrlPage} className="p-14px-ccc">Modifier l'url</p>
+			<div className="display-flex">
+				<p className="p-14px-ccc">Commenter</p>
+				<p className="p-14px-ccc">Modifier le post</p>
+				<p className="p-14px-ccc">Supprimer le post</p>
+				<p onClick={ModifierTitrePageFA} className="p-14px-ccc">Modifier le Titre</p>
+				<p onClick={ChangerMiniaturePage} className="p-14px-ccc">Changer la Miniature</p>
+				<p onClick={ModifierUrlPage} className="p-14px-ccc">Modifier l'url</p>
+			</div>
 			
 			<pre className="pre-17px-white">{titreFA}</pre>
 			<p className="p-14px-ccc">{clicFA} clic</p>
