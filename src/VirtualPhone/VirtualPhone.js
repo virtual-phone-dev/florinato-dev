93923af99c238ac17b675d8c4fc803f011715535
kilendo dev <kilendo mail>
2026-03-06 06:49:24 +0100

    bon 1

--- src/VirtualPhone/VirtualPhone.js
+++ src/VirtualPhone/VirtualPhone.js
@@ -40814,8 +40814,7 @@ async function CloseConnexionPageAA() {
   // ça c'est l'application X (page d'accueil)
   //const [xApp, setXApp] = useState(false); 
   async function XApp() {
-	const idPersonConnectedFA = localStorage.getItem("idPersonConnectedFA");
-	
+	  
     if (idPersonConnectedFA) { // si la personne est connecté , on affiche pas la page d'inscription
       setFlorinatoApp(true);
       setTelephoneVirtuel(false);
