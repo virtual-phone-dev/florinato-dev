7836cf50f8b444ef70f8e4783db3f8ac48c39e86
kilendo dev <kilendo mail>
2026-03-25 07:46:15 +0100

    bon 1

--- src/VirtualPhone/VirtualPhone.js
+++ src/VirtualPhone/VirtualPhone.js
@@ -50682,7 +50682,7 @@ function rechargerPage() {
                 <div className="a"> <img src={photoCompteConnecter} alt=""/> </div>
 
                 <div className="b">
-                  <div className="aa"> <p>{nomCompteConnecter} 0713</p> </div>
+                  <div className="aa"> <p>{nomCompteConnecter} 0747</p> </div>
                   <div className="bb"> <SvgPopularity/> <p>Popularité</p> </div>
                   <div className="cc"> <p>{populariteCompteConnecter} visites</p> </div>
                 </div>
